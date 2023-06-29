a59eb69916f8cb74e3f20ee0b9ae0b54cc4bc37d
JungInRhee <ysrheee@kaist.ac.kr>
2023-06-29 19:19:16 +0900

    stub for bruteforce solverw

---
+++ src/solver/bruteforce.rs
@@ -0,0 +1,11 @@
+use crate::board::Board;
+
+use super::NonogramSolver;
+
+struct BruteforceSolver;
+
+impl NonogramSolver for BruteforceSolver {
+    fn solve(&self) -> Option<Board> {
+        todo!()
+    }
+}
--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,5 +1,6 @@
 use crate::board::Board;
 
+pub mod bruteforce;
 pub mod sat;
 
 pub trait NonogramSolver {
